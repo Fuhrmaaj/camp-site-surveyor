9a768cc6fc69b9182daed17c64f2e7c08e377962
Fuhrmaaj <Fuhrmaaj@users.noreply.github.com>
2022-06-14 20:23:46 -0400

    feat: Add campsite component
    *Create a component below the default page, from which the campsite page can be built out
    
    Reviewed-by: @FranceJ

---
+++ src/campsite-info/campsite-info.component.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent } from "react";
+
+export const CampsiteInfo: FunctionComponent = () => {
+    return <>foo</>;
+};
---
+++ src/campsite-info/index.ts
@@ -0,0 +1,1 @@
+export {CampsiteInfo} from './campsite-info.component';
