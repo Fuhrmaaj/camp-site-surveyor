90ae4e630eec5b1ad0f3c672df8bd439ee5c1a8d
Fuhrmaaj <fuhrmaaj@gmail.com>
2022-06-18 21:58:38 -0400

    Remove accidentally committed merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,7 @@ import {CampsiteInfo} from './campsite-info';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-=======
       <CssBaseline />
->>>>>>> eebf539377b440e8a5f24e65ac7e23233f54ac5f
       <CampsiteInfo />
     </div>
   );
