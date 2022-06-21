8daeab6216eb95904dc27d226c238239d99ff7f3
Fuhrmaaj <fuhrmaaj@gmail.com>
2022-06-21 12:02:18 -0400

    add styles to the image carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
 import { CampsiteInfo } from './campsite-info';
-import { CampImages } from './campsite-info/components/camp-images';
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
       <CampsiteInfo />
-      <CampImages />
     </div>
   );
 }
---
+++ src/campsite-info/campsite-info.styles.ts
@@ -0,0 +1,18 @@
+export const graphicsStyles = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  maxWidth: "1400px",
+  margin: "auto",
+  padding: "4px"
+}
+
+export const mapStyles = {
+  width: "50%",
+  padding: "2px"
+}
+
+export const imageStyles = {
+  width: "50%",
+  padding: "2px"
+}
