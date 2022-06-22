331b299823bbe162cf71ccc98735c2cf5c7a56d8
Fuhrmaaj <fuhrmaaj@gmail.com>
2022-06-22 15:43:38 -0400

    add star rating component in the header

---
+++ src/campsite-info/components/camp-rating/camp-rating.component.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent, useState } from 'react';
+import { Rating } from 'react-simple-star-rating';
+
+export const StarRating: FunctionComponent = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (rate: number) => {
+    setRating(rate)
+  }
+  return (
+    <div className="StarRating">
+      <Rating onClick={handleRating} ratingValue={rating} />
+    </div>
+  );
+};
