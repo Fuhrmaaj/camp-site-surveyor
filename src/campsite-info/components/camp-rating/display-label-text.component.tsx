457f11a3006c47fac2eed8a8b0b859e0dbbdd147
Fuhrmaaj <fuhrmaaj@gmail.com>
2022-08-05 13:14:29 -0400

    apply linting changes

--- src/campsite-info/components/camp-rating/display-label-text.component.tsx
+++ src/campsite-info/components/camp-rating/display-label-text.component.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { Box } from "@mui/material";
-import { tooltipStyles } from "./camp-rating.styles";
-import { tooltipLabels } from "./camp-rating.constants";
+import React from 'react';
+import { Box } from '@mui/material';
+import { tooltipStyles } from './camp-rating.styles';
+import { tooltipLabels } from './camp-rating.constants';
 
 export const DisplayLabelText = (value: number, hover: number) => {
   return (
-    <Box sx={tooltipStyles}>
-      {tooltipLabels[hover !== -1 ? hover : value]}
-    </Box>
+    <Box sx={tooltipStyles}>{tooltipLabels[hover !== -1 ? hover : value]}</Box>
   );
 };
