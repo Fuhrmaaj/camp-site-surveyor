f7a8db083a5a774a01eff17059474ce452186911
Fuhrmaaj <Fuhrmaaj@users.noreply.github.com>
2022-09-17 20:31:32 -0400

    feat: Add a map component for campsites (#9)
    Adds Google Map via iframe because research concluded that it is superior to the Google Map API in every method I can determine. It is much easier to set up, and since Google no longer allows the API to be used for free, it also means we don't need to connect a bank account to the project (at least just for the maps).
    
    Reviewed-by: @FranceJ

--- src/campsite-info/campsite-info.component.tsx
+++ src/campsite-info/campsite-info.component.tsx
@@ -1,21 +1,28 @@
 import React, { FunctionComponent } from 'react';
+import { Box } from '@mui/material';
 import { CampHeader } from './components/camp-header';
 import { CampImages } from './components/camp-images';
-import { Box } from '@mui/material';
+import { CampMap } from './components/camp-map';
 import { graphicsStyles, mapStyles, imageStyles } from './campsite-info.styles';
 
 export const CampsiteInfo: FunctionComponent = () => {
-    return (
-        <div>
-            <CampHeader region="region" park="park" zone="zone" site="site" logout={() => {}}/> {/* eslint-disable-line @typescript-eslint/no-empty-function */}
-            <Box sx={graphicsStyles} component="div">
-                <Box sx={mapStyles} component="div">
-                    <div /> {/* map goes here */}
-                </Box>
-                <Box sx={imageStyles} component="div">
-                    <CampImages />
-                </Box>
-            </Box>            
-        </div>
-    );
+  return (
+    <div>
+      <CampHeader
+              region="region"
+              park="park"
+              zone="zone"
+              site="site"
+              logout={() => undefined}
+      />
+      <Box sx={graphicsStyles} component="div">
+        <Box sx={mapStyles} component="div">
+          <CampMap />
+        </Box>
+        <Box sx={imageStyles} component="div">
+          <CampImages />
+        </Box>
+      </Box>
+    </div>
+  );
 };
